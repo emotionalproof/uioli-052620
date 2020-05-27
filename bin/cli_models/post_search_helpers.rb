de8ca9d688218881e31d0c62ad8f52c81fd69f98
grantkoji <grantkoji@gmail.com>
2020-05-27 19:28:06 -0400

    recipes finalized added

--- bin/cli_models/post_search_helpers.rb
+++ bin/cli_models/post_search_helpers.rb
@@ -22,6 +22,16 @@ def finalize_recipes(selected_recipes)
     " to re-select your recipes, enter: n")
 end
 
+
+def shopping_list(ingredients_array)
+    prompt.multi_select("\nHere are the results of your Use It Or Lose It Search:"\
+    "#{string_results}.\n\nWhich of these recipes would you like to"\
+        " add to your cookbook?\n\n") do |menu|
+        results_array.map do |array_element|
+            menu.choice array_element
+            end
+        end
+end
  
 
 
