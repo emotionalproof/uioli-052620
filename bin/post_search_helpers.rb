53ab34ccf3de9eb9d7233f13d094cf8e1da680de
grantkoji <grantkoji@gmail.com>
2020-05-29 09:27:11 -0400

    added AR::Base.logger = nil, moved run functions to post search helpers

--- bin/post_search_helpers.rb
+++ bin/post_search_helpers.rb
@@ -56,21 +56,67 @@ def finalize_cookbook(selected_recipe_hash)
     " to re-select your recipe, enter: n")
 end
 
+def get_source_website(api_source_website_by_recipe_id)
+    api_get = api_source_website_by_recipe_id
+    "You can read the full recipe and detailed instructions at #{api_get["sourceUrl"]}"
+end
 
-# def shopping_list(recipe_name, recipe_url, recipe_ingredients)
-#     # recipe_name = selected_recipe_hash["name"]
-#     # recipe_url = selected_recipe_hash["url"]
-#     #how do I list out the returned ingredients
-#     puts "Great, you are about to cook #{recipe_name}!"\
-#     "\n\nYou can find your recipe at #{recipe_url}"\
-#     "\n\nYour recipe may have greater quantities of ingredients than you have, so\n"\
-#     " we are including all ingredients on this list - including potential Use It Or Lose Its!"\
-#     "\n\nHere is your shopping list:"\
-#     "\n\n#{recipe_ingredients}"\
-#     "\n\nThank you!\n\nUntil the next time you fear that you will Lose It...\n\n"\
-#     "We appreciate you for choosing to Use It.\n\n"\
-# end
+def get_recipe_instructions(api_instructions_by_recipe_id)
+        instructions_array = []
+        api_get = api_instructions_by_recipe_id
+        api_get.reverse.each do |instruction|
+            instruction["steps"].each do |step|
+                instructions_array << step["step"]
+            end
+        end
+        instructions_array
+    end
 
 
+def get_recipe_ingredients(api_ingredients_by_recipe_id)
+    array = [] 
+    api_get = api_ingredients_by_recipe_id
+     api_get["ingredients"].each do |ingredient| 
+         array << "#{ingredient["amount"]["us"]["value"]} #{ingredient["amount"]["us"]["unit"]} of #{ingredient["name"]}"
+     end
+     array
+ end
 
-
+ def goodbye(selected_recipe_name, recipe_ingredients_from_api, recipe_source_from_api, recipe_instructions_from_api)
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts "Great! You are about to cook #{selected_recipe_name}!"
+    puts 
+    puts "Use It Or Lose It! will try to utilize all of the ingredients on your chopping block"
+    puts "but you may need more items before you are chowing down on #{selected_recipe_name}."
+    puts
+    puts "Below, you will find a complete list of ingredients(with quantities)"
+    puts "as well as instructions on how to prepare, cook, and serve this amazing dish."
+    puts
+    puts "INGREDIENTS:"
+    "#{recipe_ingredients_from_api.each {|item| puts item}}"
+    puts
+    puts "INSTRUCTIONS"
+    puts "#{recipe_source_from_api}"
+    "#{recipe_instructions_from_api.each {|step| puts step}}"
+    puts
+    puts
+    puts "Until the next time you fear that you will Lose It..."
+    puts "We appreciate you for choosing to Use It."
+    puts "Thank you!"
+ end
